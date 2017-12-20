4d7d168100137f6b05b776ea835ddd936d0b0f9e
Jeremy Kauffman <jkauffman024@gmail.com>
2017-12-20 12:08:28 -0500

    Step 5

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
   // your code will go here
-
+$.ajax({
+  url: https://www.codeschool.com/users/jkauffman024.json,
+  dataType: 'jsonp',
+  success: function(response){
+    // handle response
+  }
+})
 });
